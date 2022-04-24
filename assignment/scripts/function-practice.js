f6b981cb3e789353108f2a3590d02d7af0747d33
Stephen Womack <swomack111@gmail.com>
2022-04-24 13:48:26 -0500

    completed firstLetter function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -89,7 +89,10 @@ console.log(find(2, test));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string.charAt(0)) {
+    return 'true'
+  }
+  return 'false'
 }
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
